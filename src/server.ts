c9e5cbc6e1383a64773dc05dbd826201d98d7cea
Ricardo Colares <ricardo.colares@hp.com>
2024-04-10 08:12:11 -0300

    chore: eslint configured

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
-import { app } from "./app"
-import { env } from "./env"
+import { app } from './app'
+import { env } from './env'
 
-app.listen({
-  host: '0.0.0.0',
-  port: env.PORT
-}).then(() => {
-  console.log('🚀HTTP Server Running!')
-})
+app
+  .listen({
+    host: '0.0.0.0',
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('🚀HTTP Server Running!')
+  })
+
+export const a = 'a'
