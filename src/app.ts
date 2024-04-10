25de35bdfe8489d908e18b15c60eac319db95f74
Ricardo Colares <ricardo.colares@hp.com>
2024-04-10 08:51:02 -0300

    feat: db configured with prisma and postgresql

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'Ricardo',
+    email: 'ricardo@mail.com',
+  },
+})
 
 export const app = fastify()
