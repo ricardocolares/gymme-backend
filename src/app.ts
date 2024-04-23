672e06121f551dcda1dae48b3c1579af6637a76d
Ricardo Colares <ricardoccolares@gmail.com>
2024-04-23 14:30:36 -0300

    refactor: error handeling

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,20 @@
 import fastify from 'fastify'
 import { appRoutes } from './http/routes'
+import { ZodError } from 'zod'
+import { env } from './env'
 
 export const app = fastify()
 
 app.register(appRoutes)
+
+app.setErrorHandler((err, _, reply) => {
+    if (err instanceof ZodError) {
+        return reply.status(400).send({ message: 'Validation error.', issues: err.format() })
+    }
+
+    if (env.NODE_ENV !== 'production') { console.error(err) } else {
+        //TODO: WE SHOULD LOG TO AN EXTERNAL TOOL LIKE SENTRY
+    }
+
+    return reply.status(500).send({ message: 'Internal Server Error' })
+})
--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,5 +1,6 @@
 import { hash } from 'bcryptjs'
 import { UsersRepository } from '@/repositories/users-repository'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
 
 interface RegisterUseCaseRequest {
   name: string
@@ -7,16 +8,14 @@ interface RegisterUseCaseRequest {
   password: string
 }
 export class RegisterUseCase {
-  constructor(private usersRepository: UsersRepository) {}
+  constructor(private usersRepository: UsersRepository) { }
 
   async execute({ name, email, password }: RegisterUseCaseRequest) {
     const password_hash = await hash(password, 6)
 
     const userWithSameEmail = await this.usersRepository.findByEmail(email)
 
-    if (userWithSameEmail) {
-      throw new Error('User with same email already exists')
-    }
+    if (userWithSameEmail) throw new UserAlreadyExistsError
 
     await this.usersRepository.create({
       email,
